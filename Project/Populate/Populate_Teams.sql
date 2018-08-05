a38f164690192d6d16efa1164045820af5ca2256
lior-za <lior_za@hotmail.com>
2018-08-05 00:31:10 -0400

    Added 3 new populate scripts and two CSV's

---
+++ Project/Populate/Populate_Teams.sql
@@ -0,0 +1,7 @@
+Insert into Teams (TeamID, ManagerID) values (1,1); 
+Insert into Teams (TeamID, ManagerID) values (2,2); 
+Insert into Teams (TeamID, ManagerID) values (3,6); 
+Insert into Teams (TeamID, ManagerID) values (4,7); 
+Insert into Teams (TeamID, ManagerID) values (5,12); 
+Insert into Teams (TeamID, ManagerID) values (6,13); 
+Insert into Teams (TeamID, ManagerID) values (7,19); 
