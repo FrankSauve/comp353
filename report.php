7f18b8d0c1566cab1e961f775183ba7f756b5cec
FrankSauve <franksauve@outlook.com>
2018-08-10 18:59:19 -0400

    Second required query

--- report.php
+++ report.php
@@ -13,8 +13,11 @@
             margin-right: 200px;
             margin-bottom: 30px;
         }
-        #title {
-            margin-bottom: 30px;
+        .subtitle {
+            margin-top: 30px;
+        }
+        .data{
+            margin-left:50px;
         }
     </style>
 </head>
@@ -36,9 +39,16 @@
 ?>
 <body>
     <div id="container">
-        <h1 id="title">Report</h1>
-        <p>Number of employees with <strong>Premium</strong> employee plan with working hours <strong>less than 60 hrs/month</strong></p>
+        <h1>Report</h1>
+        <p class="subtitle">Number of employees with <strong>Premium</strong> employee plan with working hours <strong>less than 60 hrs/month</strong></p>
+        <div class="data">
         <?php
+        // Get all employees with Premium insurance
+        $sql = "SELECT EmployeeID, fName, lName FROM Employees WHERE InsuranceID=(SELECT InsuranceID FROM Insurance WHERE TypeDescript='Premium')";
+        $result = $conn->query($sql);
+
+        $count = 0;
+        // Get sum of hours of all employees with insurance plan
         echo "<table border='1'>
             <tr>
             <th>EmployeeID</th>
@@ -46,12 +56,6 @@
             <th>Last Name</th>
             <th>Hours</th>
             </tr>";
-
-        // Get all employees with Premium insurance
-        $sql = "SELECT EmployeeID, fName, lName FROM Employees WHERE InsuranceID=(SELECT InsuranceID FROM Insurance WHERE TypeDescript='Premium')";
-        $result = $conn->query($sql);
-
-        // Get sum of hours of all employees with insurance plan
         while ($row = $result->fetch_assoc()) {
             $employeeID = $row['EmployeeID'];
             $sql = "SELECT SUM(hours) FROM Hours WHERE EmployeeID='$employeeID'";
@@ -64,6 +68,7 @@
                 $hours = 0;
             }
             if ($hours < 60){
+                $count++;
                 echo "<tr>";
                 echo "<td>" . $row['EmployeeID'] . "</td>";
                 echo "<td>" . $row['fName'] . "</td>";
@@ -73,7 +78,35 @@
             }
         }
         echo "</table>";
+        echo "<strong>Total count: </strong>" . $count;
         ?>
+        </div>
+
+        <p class="subtitle">Number of Premium contracts delivered in more than <strong>10 business days</strong> having more than 35 employees with <strong>Silver Employee Plan</strong></p>
+        <div class="data">
+        <?php
+            $count2 = 0;
+            //Get all premium contracts with a ThirdDeliv date
+            $sql =  "SELECT ThirdDeliv, StartDate, ContID FROM Contracts WHERE Category='Premium' AND ThirdDeliv <> ''";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                // Difference between start date and third delivery date
+                $deliveryTime = date_diff(date_create($row['ThirdDeliv']), date_create($row['StartDate']));
+                $contID = $row['ContID'];
+                // If more than 10 days
+                if ($deliveryTime->d > 10) {
+                    $sql = "SELECT COUNT(DISTINCT EmployeeHistory.EmployeeID) FROM EmployeeHistory, Employees WHERE contID ='$contID' AND Employees.InsuranceID=(SELECT InsuranceID FROM Insurance WHERE TypeDescript='Premium') AND Employees.EmployeeID=EmployeeHistory.EmployeeID;";
+                    $r = $conn->query($sql);
+                    $r2 = $r->fetch_assoc();
+                    $numEmployees = $r2['COUNT(DISTINCT EmployeeHistory.EmployeeID)'];
+                    if($numEmployees > 35) {
+                        $count2++;
+                    }
+                }
+            }
+            echo "<strong>Total count: </strong>" . $count2;
+        ?>
+        </div>
     </div>
 </body>
 </html>
